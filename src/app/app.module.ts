30aad7981ea5a1336b8075ff161926f7074da4b6
iftikharyk <iftikharyk@gmail.com>
2020-07-24 17:51:59 +0500

    topbar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
