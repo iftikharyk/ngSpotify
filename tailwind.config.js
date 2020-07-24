ba118d61f28d1e9b7c7a48e05df7a32eb34a5007
iftikharyk <iftikharyk@gmail.com>
2020-07-24 10:33:10 +0500

    sidebar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-900-spotify': '#121212',
+        'gray-800-spotify': '#181818',
+        'gray-700-spotify': '#282828'
+      }
+    },
   },
-  variants: {},
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover']
+  },
   plugins: [],
 }
